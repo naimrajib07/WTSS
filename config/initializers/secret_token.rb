70310aab75a0a04d515cb672f3c291c4db098261
Naim Rajib <naim_cse07@yahoo.com>
2014-12-06 11:15:18 +0600

    Base Backend Application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WTSS::Application.config.secret_token = '65d3e0bde972eee6c45b5f2f7c0a6504f650fc57d5ac5cf5a82de262f372abcdbc444db95354500594b923de028d02b4a9ae34ca6e93f7fdbd13e50771606b74'
